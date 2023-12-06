da775dda932ba3954c09319e1fe075ed31e8e20a
arad4228 <arad4228@gmail.com>
2023-12-06 10:05:56 +0900

    [KyoungJae]Add srtp GStreamer

--- ReceiverVideo.cpp
+++ ReceiverVideo.cpp
@@ -2,11 +2,26 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
 
-int main() {
+int main() 
+{
+    // 30~50 bytes
+    std::string strTestkey = "012345678901234567890123456789012345678901234567890123456789";
+
     // GStreamer 파이프라인을 통한 UDP 스트림 수신 설정
-    std::string pipeline = "udpsrc port=5000 "
-                           "! application/x-rtp, media=video, encoding-name=H264, payload=96"
-                           "! queue ! rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! appsink sync=false";
+    // Nomal
+    // std::string pipeline = "udpsrc port=5000 "
+    //                       "! application/x-rtp, media=video, encoding-name=H264, payload=96"
+    //                       "! queue ! rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! appsink sync=false";
+
+    // Enc
+    std::string pipeline = 
+        "udpsrc port=5000 ! "
+        "application/x-srtp, media=video, clock-rate=90000, encoding-name=H264, payload=96, ssrc=(uint)1356955624, "
+        "srtp-key=(buffer)" + strTestkey + ", "
+        "srtp-cipher=aes-128-icm, srtp-auth=hmac-sha1-80 , srtcp-cipher=aes-128-icm, srtcp-auth=hmac-sha1-80 ! "
+        "srtpdec ! rtph264depay !"
+        "avdec_h264 ! videoconvert ! queue ! "
+        "appsink sync=false";
 
     // VideoCapture 객체를 GStreamer 파이프라인을 사용하여 열기
     cv::VideoCapture cap(pipeline, cv::CAP_GSTREAMER);
--- SendVideo.cpp
+++ SendVideo.cpp
@@ -1,12 +1,12 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
+#include <string>
 
 using namespace cv;
 
-int main(int argc, char** argv) 
+int main() 
 {
-    std::string strIPAddress(argv[1]);
     cv::VideoCapture cap(0); // 라즈베리 파이 카메라 모듈 열기
 
     if (!cap.isOpened()) {
@@ -17,9 +17,21 @@ int main(int argc, char** argv)
     cap.set(cv::CAP_PROP_FRAME_WIDTH, 640);
     cap.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
     cap.set(cv::CAP_PROP_FOURCC, cv::VideoWriter::fourcc('H', '2', '6', '4'));
+    std::string strTestkey = "012345678901234567890123456789012345678901234567890123456789";
 
     // GStreamer 파이프라인
-    std::string pipeline = "appsrc ! queue ! videoconvert ! x264enc ! rtph264pay ! udpsink host="+strIPAddress+" port=5000";
+    // Nomal
+    // std::string pipeline = "appsrc ! queue ! videoconvert ! x264enc ! rtph264pay ! udpsink host=163.152.126.107 port=5000";
+    
+    // Enc
+    std::string pipeline =
+        "appsrc ! "
+        "videoconvert ! "
+        "video/x-raw, width=640, height=480, framerate=30/1, format=I420 ! "
+        "x264enc tune=zerolatency ! "
+        "rtph264pay ssrc=1356955624 ! "
+        "srtpenc key=" + strTestkey + " ! queue ! udpsink sync=false host=163.152.126.107 port=5000";
+
     cv::VideoWriter writer;
     writer.open(pipeline, 0, 30, cv::Size(640, 480), true);
 
@@ -30,15 +42,13 @@ int main(int argc, char** argv)
 
     while (true) 
     {
-	cv::Mat frame;
-	cap >> frame;
-        
+	    cv::Mat frame;
+	    cap >> frame;
         if (frame.empty()) 
         {
             std::cerr << "Frame Empty" << std::endl;
             break;
         }
-        // cv::imshow("test.avi", frame);
         writer << frame;
     }
 
